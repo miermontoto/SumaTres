06420cfe9386411adfadf787e620a90508635d36
GijonDev <underscoreBis@gmail.com>
2020-12-22 14:15:38 +0100

    v3

--- SumaTres.java
+++ SumaTres.java
@@ -1,13 +1,12 @@
 import static java.lang.System.out;
 import java.util.Random;
 public class SumaTres {
-	//el tamaño del tablero se establece en los constructores
+
 	private int sizeX;
-	private int sizeY;
+	private int sizeY; //el tamaño del tablero se establece en los constructores
 	private int turnos = 0; //no es necesario
 	private int puntuación = 0; //la partida se comienza con 0 puntos
 	private int siguiente = 1; //la primera "siguiente" es 1.
-
 
 	public SumaTres() { //el constructor por defecto establece 5x5 (como en las demos) para el tablero
 		this(5, 5);
@@ -43,7 +42,6 @@ public class SumaTres {
 		//para izquierda, el desplazamiento es -1 en la horizontal
 		//utilizando esto, se reduce el código en un 70% aprox.
 
-
 		switch(c) {
 		case 'a': //ARRIBA
 			movy--;
@@ -68,28 +66,28 @@ public class SumaTres {
 
 		if(movx != 0 && movy != 0) { //si no se desplaza el índice significa que no es una jugada válida.
 			for(int i=0; i<sizeX; i++) for(int j=0; j<sizeY; j++) { //búsqueda de casillas que contengan valores.
-				/*if(getTab(i, j)!=0) {
-					if(getTab(i+movx, j+movy)==0) { 
-						setTab(i+movx, j+movy, getTab(i, j));
-						setTab(i, j, 0);
-						
-					} else {
-						if(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) {
-							setTab(i+movx, j+movy, getTab(i, j) + getTab(i+movx, j+movy));
-							setTab(i, j, 0);
-							
-						}
-					}*/
-				
-				//TODO probar que esto funciona
-				//Documentación: elimina todos los if y else en favor de reducción de código.
-				//Incluye las comprobaciones del movimiento originales:
 				// si la casilla destino está vacía, simplemente se mueve.
 				// si la casilla destino es igual o suman 3 (1+2 || 2+1), se suman.
 				// *TODO comprobar casos de varias casillas iguales juntas
+				if(getTab(i, j)!=0) {
+					if(getTab(i+movx, j+movy)==0) { 
+						setTab(i+movx, j+movy, getTab(i, j));
+						setTab(i, j, 0);
+					} else {
+						if(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) {
+							setTab(i+movx, j+movy, getTab(i, j) + getTab(i+movx, j+movy));
+							setTab(i, j, 0);	
+						}
+					}
+				}
+
+				//beta sin if's
+				//TODO probar que esto funciona
+				//Documentación: elimina todos los if y else en favor de reducción de código.
+				//Incluye las comprobaciones del movimiento originales
 				//*puede que favorecer la reduccción frente a la estética y simplicidad del código no merezca la pena.
-				
-				
+
+				/*
 				setTab(i+movx, j+movy, 
 					(getTab(i+movx, j+movy)==0)? getTab(i, j) :
 						(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3)?
@@ -97,6 +95,7 @@ public class SumaTres {
 				setTab(i, j,
 						(getTab(i+movx, j+movy)==0) || (getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) ?
 								0 : getTab(i, j));
+				 */
 
 			}
 		}
@@ -116,5 +115,6 @@ public class SumaTres {
 	public String toString() {
 		String salida = String.format(""); //TODO completar salida para que coincida con la demo
 		return salida; //tiene que incluir "Siguiente ficha: " y "Puntos: "
+		//imprimir los turnos???
 	}
 }
