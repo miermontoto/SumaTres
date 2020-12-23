8f6d0b91696eddca96635672b9ec8934c147b10a
GijonDev <underscoreBis@gmail.com>
2020-12-24 00:02:36 +0100

    limpieza, TODOs, comentarios y planes

--- SumaTres.java
+++ SumaTres.java
@@ -2,23 +2,39 @@ import static java.lang.System.out;
 import java.util.Random;
 public class SumaTres {
 
+	// TODO lógica de suma completa
+	// TODO nueva ficha cada jugada
+	// TODO imprimir matriz
+	// TODO main
+	// TODO puntos
+	// TODO comprobar final de partida
+	// TODO JavaDoc
+	// TODO documentación pdf
+	// TODO reestructuración y comentarios
+	// TODO limpieza de código
+	// TODO casos extremos
+	// TODO ¿más setters y getters?
+
+
 	private int sizeX;
 	private int sizeY; //el tamaño del tablero se establece en los constructores
 	private int turnos = 0; //no es necesario
-	private int puntuación = 0; //la partida se comienza con 0 puntos
-	private int siguiente = 1; //la primera "siguiente" es 1.
+	private int siguiente = 1;
+	private int[][] tablero = new int[sizeX][sizeY];
+	private int puntos = 0;
 
 	public SumaTres() { //el constructor por defecto establece 5x5 (como en las demos) para el tablero
 		this(5, 5);
 	}
 
 	public SumaTres(int x, int y) {
-		this.sizeX = x;
-		this.sizeY = y;
+		if(x>=3 && y>=3) {this.sizeX = x; this.sizeY = y;}
+		else {this.sizeX = 5; this.sizeY = 5;}
+		// se presupone que el tamaño mínimo del tablero es 3x3
+		// de no ser así, se establece el tamaño por defecto.
 	}
 
-	private int[][] tablero = new int[sizeX][sizeY];
-	private int puntos = 0;
+
 
 	// --- sets y gets --- //
 	public int getPuntos() {return this.puntos;}
@@ -26,9 +42,6 @@ public class SumaTres {
 
 	public int getTab(int x, int y) {return this.tablero[x][y];}
 	public void setTab(int x, int y, int nv) {this.tablero[x][y] = nv;}
-
-	public int getPuntuación() {return this.puntuación;}
-	public void setPuntuación(int puntuación) {this.puntuación = puntuación;}
 
 	public int getSiguiente() {return this.siguiente;}
 	public void setSiguiente(int siguiente) {this.siguiente = siguiente;}
@@ -64,40 +77,38 @@ public class SumaTres {
 			break;
 		}
 
+		// si la casilla destino está vacía, simplemente se mueve.
+		// si la casilla destino es igual o suman 3 (1+2 || 2+1), se suman.
+		// se podría simplificar si se utilizara un solo conjunto de bucles for,
+		// sumando y moviendo a la vez, pero eso no se asemeja con la demo.
+		//
+		// TODO comprobar casos de varias casillas iguales juntas.
+		// TODO imprimir la matriz movida antes de sumar elementos.
+		// TODO comprobar el orden de suma de las casillas.
+
 		if(movx != 0 && movy != 0) { //si no se desplaza el índice significa que no es una jugada válida.
 			for(int i=0; i<sizeX; i++) for(int j=0; j<sizeY; j++) { //búsqueda de casillas que contengan valores.
-				// si la casilla destino está vacía, simplemente se mueve.
-				// si la casilla destino es igual o suman 3 (1+2 || 2+1), se suman.
-				// *TODO comprobar casos de varias casillas iguales juntas
+				// Paso 1: Se mueven las fichas en la dirección adecuada antes de sumar nada.
 				if(getTab(i, j)!=0) {
 					if(getTab(i+movx, j+movy)==0) { 
 						setTab(i+movx, j+movy, getTab(i, j));
 						setTab(i, j, 0);
-					} else {
-						if(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) {
-							setTab(i+movx, j+movy, getTab(i, j) + getTab(i+movx, j+movy));
-							setTab(i, j, 0);	
-						}
 					}
 				}
-
-				//beta sin if's
-				//TODO probar que esto funciona
-				//Documentación: elimina todos los if y else en favor de reducción de código.
-				//Incluye las comprobaciones del movimiento originales
-				//*puede que favorecer la reduccción frente a la estética y simplicidad del código no merezca la pena.
-
-				/*
-				setTab(i+movx, j+movy, 
-					(getTab(i+movx, j+movy)==0)? getTab(i, j) :
-						(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3)?
-								getTab(i, j) + getTab(i+movx, j+movy) : getTab(i+movx, j+movy));
-				setTab(i, j,
-						(getTab(i+movx, j+movy)==0) || (getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) ?
-								0 : getTab(i, j));
-				 */
-
 			}
+			
+			//out.println(this);
+
+			for(int i=0; i<sizeX; i++) for(int j=0; j<sizeY; j++) {
+				if(getTab(i, j)!=0) { // como antes, se buscan casillas que contengan valores.
+					if(getTab(i, j) == getTab(i+movx, j+movy) || getTab(i, j) + getTab(i+movx, j+movy) == 3) {
+						setTab(i+movx, j+movy, getTab(i, j) + getTab(i+movx, j+movy));
+						setTab(i, j, 0);	
+					}
+				}
+			}
+			
+			//out.println(this);
 		}
 	}
 
