13e62fbc122fbf689b02588512dc19a6c7464ec1
miermontoto <UO283319@uniovi.es>
2022-03-11 22:28:34 +0100

    javadoc en handler.Keyboard

--- src/handler/Keyboard.java
+++ src/handler/Keyboard.java
@@ -1,12 +1,18 @@
 package handler;
 import game.SumaTres;
-import java.awt.List;
 
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-
+/**
+ * Clase manejadora de eventos de teclado del programa SumaTres. <p>
+ * El funcionamiento de todos los objetos de esta clase se puede desactivar
+ * mediante el flag {@code handlingEnabled}, a través del método
+ * {@link disableHandling()}.
+ * 
+ * @see game.SumaTres
+ * @see game.LauncherRF
+ * @author Juan Mier
+ */
 public class Keyboard {
 	
 	public static final String VALID_CLASSIC_KEYS = "wasd";
@@ -18,25 +24,61 @@ public class Keyboard {
         
         public static void disableHandling() {handlingEnabled = false;}
 
+        /**
+         * Constructor por defecto. Requiere el objeto de la partida que se está
+         * jugando y el evento de teclado generado.
+         * 
+         * @see SumaTres
+         * @see KeyEvent
+         * 
+         * @param si Objeto de la clase SumaTres de la partida actual.
+         * @param ei Evento de teclado que se quiera tratar.
+         */
 	public Keyboard(SumaTres si, KeyEvent ei) {
             s = si;
             e = ei;
 	}
 	
+        /**
+         * Método principal de la clase. Realiza jugadas en base a la tecla pulsada.
+         * Las teclas válidas se seleccionan mediante {@link validKeychar()}
+         * y {@link validKeycode()}. <p>
+         * Traduce las jugadas por flechas mediante {@link translateKeycode()}.
+         */
 	public void keyboardHandler() {
-            if(validKeystroke() && handlingEnabled) s.jugada(e.getKeyChar());
+            if(validKeychar() && handlingEnabled) s.jugada(e.getKeyChar());
             else if(validKeycode() && handlingEnabled) s.jugada(translateKeycode());
 	}
         
-        private boolean validKeystroke() {
+        /**
+         * Método que define si el evento de una acción de teclado se corresponde
+         * con el de una jugada válida. En el modo clásico, las teclas válidas son
+         * "wasd". En modo experimental, son "qweadzxc". <p>
+         * Para validar el caracter, busca su índice en unas constantes de la clase.
+         * 
+         * @return Valor booleano que define la validez.
+         */
+        private boolean validKeychar() {
             return (s.getSettings().isDiagonalMovementEnabled() && (VALID_EXPERIMENTAL_KEYS.indexOf(e.getKeyChar())) >= 0) ||
                     (!s.getSettings().isDiagonalMovementEnabled() && (VALID_CLASSIC_KEYS.indexOf(e.getKeyChar()) >= 0));
         }
         
+        /**
+         * Método que define si el evento de una acción de teclado se corresponde
+         * con el de una tecla válida. Actualmente solo se utilizan las flechas,
+         * por lo que devolverá cierto si la tecla pulsada fue una flecha.
+         * @return Valor booleano que define la validez.
+         */
         private boolean validKeycode() {
             return e.getKeyCode() <= 40 && e.getKeyCode() >= 37;
         }
         
+        /**
+         * Método que traduce los eventos de teclados correspondientes de las
+         * flechas a "wasd". Permite traducir jugadas al sistema ya en uso.
+         * 
+         * @return Caracter correspondiente a la dirección del movimiento.
+         */
         private char translateKeycode() {
             int code = e.getKeyCode();
             if(code == KeyEvent.VK_UP) return 'w';
